fb4fcde853fe3b75ea4f46dca65cf738aa1d57eb
msaidcevik <m.s.cevik23@gmail.com>
2022-07-22 22:17:34 +0300

    variable.tf added

---
+++ aws/variable/variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_ami" {
+  default = "ami-0cff7528ff583bf9a"
+}
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_name" {
+  default = "said-ec2"
+}
+
+variable "s3_bucket_name" {
+  default = "said-s3-bucket"
+}
