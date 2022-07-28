dcd5125ee71bdc4720c4e47dc394f92a01e6a986
msaidcevik <m.s.cevik23@gmail.com>
2022-07-28 15:25:37 +0300

    recent_ami file updated

--- aws/aws_ec2/recent_ami/main.tf
+++ aws/aws_ec2/recent_ami/main.tf
@@ -11,19 +11,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "tf_ami" {
+data "aws_ami" "amazon-linux-2" {
+  owners      = ["amazon"]
   most_recent = true
-  owners = ["self"]
 
   filter {
-    name = "virtualization-type"
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
     values = ["hvm"]
   }
-  
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*"]
+  }
 }
 
 resource "aws_instance" "tf-ec2" {
-  ami           = data.aws_ami.tf_ami.id
+  ami           = data.aws_ami.amazon-linux-2.id
   instance_type = var.ec2_type
   key_name      = var.key_name
   tags = {
